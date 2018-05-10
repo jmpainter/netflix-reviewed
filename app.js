93a011874aaf1e391a7a32b688de38d2a26d4f4e
jmpainter <jpainter4@gmail.com>
2018-05-10 16:03:36 -0700

    Adding reviews to movie data

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const UNOGS_API_URL = 'https://unogs-unogs-v1.p.mashape.com/aaapi.cgi'
+const UNOGS_API_URL = 'https://unogs-unogs-v1.p.mashape.com/aaapi.cgi';
+const OMDB_API_URL = 'http://www.omdbapi.com' ;
 
-function getDataFromApi() {
+const appState = {
+  movies: null,
+  movieCounter: 0
+};
+
+function getMovieList() {
 
   let daysBack = '7';
   let countryId = 'US';
@@ -12,7 +18,7 @@ function getDataFromApi() {
     dataType: 'json',
     contentType: 'application/json; charset=utf-8',
     beforeSend: setHeader
-  }).done(data => console.log(data))
+  }).done(data => addReviewsToData(data))
     .fail(function (jqXHR, exception) {
       // Our error logic here
       var msg = '';
@@ -40,7 +46,76 @@ function getDataFromApi() {
   }
 }
 
+function getReviews(imdbid) {
+  const data = {
+    i: imdbid,
+    apikey: 'dc59eece'
+  };
+  $.ajax({
+    url: OMDB_API_URL,
+    data: data,
+    type: 'GET',
+    dataType: 'json',
+  }).done(detail => {
+    setReviewsForMovie(imdbid, detail.Ratings);
+  }).fail((jqXHR, exception) => {reviews = null});
+}
+
+function setReviewsForMovie(imdbid, reviews) {
+  let movieIndex = appState.movies.findIndex(movie => movie.imdbid === imdbid);
+  let reviewImdb = null;
+  let reviewRt = null;
+  let reviewMetacritic = null;
+  if(reviews) {
+    reviews.forEach(review => {
+      if(review.Source === 'Internet Movie Database') {
+        reviewImdb = review.Value;
+      } else if (review.Source === 'Rotten Tomatoes') {
+        reviewRt = review.Value;
+      } else if (review.Source === 'Metacritic') {
+        reviewMetacritic = review.Value;
+      }
+    });
+  }
+  appState.movies[movieIndex]['review-imdb'] = reviewImdb;
+  appState.movies[movieIndex]['review-rt'] = reviewRt;
+  appState.movies[movieIndex]['review-metacritic'] = reviewMetacritic;
+  appState.movieCounter++;
+  if(appState.movieCounter === appState.movies.length) {
+    console.log('FINISHED!');
+    console.log(appState.movies);
+  }
+}
+
+// :
+// {Source: "Internet Movie Database", Value: "6.5/10"}
+// 1
+// :
+// {Source: "Rotten Tomatoes", Value: "33%"}
+// 2
+// :
+// {Source: "Metacritic", Value: "37/100"}
+
+function addReviewsToData(data) {
+  // console.log(data);
+  appState.movies = data.ITEMS;
+  // console.log(appState.movies);
+  // let reviews = null;
+  // let length = data.ITEMS.length;
+  // // console.log(appState.movies.length);
+  // for(let i = 0; i < length; i++) {
+  //   reviews = getReviews(data.ITEMS[i].imdbid);
+  //   // console.log(reviews);
+  // };
+  let reviews = null;
+  // console.log(appState.movies);
+  appState.movies.forEach(movie => {
+    reviews = getReviews(movie.imdbid);
+  });
+  
+}
+
 function startApp() {
-  getDataFromApi();
+  getMovieList();
 }
 $(startApp);
